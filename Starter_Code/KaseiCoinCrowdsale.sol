3dbd63f7faca8de98f280dcd1c78cd9658fd316f
ariana <ariana.moreno13@yahoo.com>
2023-03-26 14:39:08 -0600

    folder structure kaseicoincrowdsale.sol updated. updated new instances and assigned contracts to addresses.

--- Starter_Code/KaseiCoinCrowdsale.sol
+++ Starter_Code/KaseiCoinCrowdsale.sol
@@ -30,19 +30,22 @@ contract KaseiCoinCrowdsaleDeployer {
     constructor(
        string memory name,
        string memory symbol,
-       address payable wallet
+       address payable wallet // this wallet will collect all the ether raised by the sale.
     ) public {
         // Create a new instance of the KaseiCoin contract.
-        // YOUR CODE HERE!
+        KaseiCoin token = new KaseiCoin(name, symbol, 0);
+        
         
         // Assign the token contract’s address to the `kasei_token_address` variable.
-        // YOUR CODE HERE!
+        kasei_token_address = address(token);
+        
 
         // Create a new instance of the `KaseiCoinCrowdsale` contract
-        // YOUR CODE HERE!
-            
+        KaseiCoinCrowdsale kasei_crowdsale = new KaseiCoinCrowdsale(1, wallet, token);
+        
+    
         // Aassign the `KaseiCoinCrowdsale` contract’s address to the `kasei_crowdsale_address` variable.
-        // YOUR CODE HERE!
+        kasei_crowdsale_address = address(kasei_crowdsale);
 
         // Set the `KaseiCoinCrowdsale` contract as a minter
         // YOUR CODE HERE!
